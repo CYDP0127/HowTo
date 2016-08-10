dc05e7a58f950b5117a7d1f83d94c67dcac73a26
Daniel Choi <cydp0127@gmail.com>
2016-08-10 21:17:25 +0900

    Add hw to flip div tag
    Add hw to flip div tag

---
+++ Web/FlipDiv/sameple.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Daniel
+ * Date: 8/10/2016
+ * Time: 9:14 PM
+ */
+
+?>
+<!------------jquery import ----------->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js" type="text/javascript" charset="utf-8"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js" type="text/javascript" charset="utf-8"></script>
+
+
+<link rel="stylesheet" href="css/style.css">
+
+<div class="wrapper" id="wrapper">
+    <div class="container">
+        <div class="section">
+            <div class="section__content clearfix">
+                <div class="card effect__click">
+                    <div class="card__front">
+                                <span class="card__text">
+                                    front
+                                </span>
+                    </div>
+                    <div class="card__back">
+                                <span class="card__text">
+                                    back
+
+                                </span>
+                    </div>
+                </div><!-- /card -->
+            </div>
+        </div><!-- /card -->
+    </div><!-- /#wrapper -->
+</div>
